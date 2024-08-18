4053c048ba1a0ae0701e2f42d165be28755b8aee
miltonbhowmick <miltonbhowmick6@gmail.com>
2024-08-19 00:52:15 +0600

    feat: add app by app urls

--- server/core/settings/base.py
+++ server/core/settings/base.py
@@ -51,6 +51,10 @@ INSTALLED_APPS = [
     "home",
     "search",
     "account",
+    "common",
+    "music",
+    "payment",
+    "order",
 ]
 
 MIDDLEWARE = [
@@ -115,6 +119,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
     ),
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 20,
 }
--- server/core/urls.py
+++ server/core/urls.py
@@ -14,6 +14,8 @@ urlpatterns = [
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
     path("api/v1/account/", include("account.urls", namespace="account")),
+    path("api/v1/common/", include("common.urls", namespace="common")),
+    path("api/v1/music/", include("music.urls", namespace="music")),
 ]
 
 
