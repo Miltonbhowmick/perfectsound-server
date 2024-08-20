9f087a72a137731d42ac0063461b8b72d2b49797
miltonbhowmick <miltonbhowmick6@gmail.com>
2024-08-20 20:59:56 +0600

    test: try with wagtail footer global setting from admin

--- server/common/models.py
+++ server/common/models.py
@@ -3,6 +3,10 @@ from django.utils.translation import gettext_lazy as _
 from account.models import User
 from django.utils.text import slugify
 
+from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.snippets.models import register_snippet
+
 
 class Category(models.Model):
     name = models.CharField(_("Name"), max_length=255, blank=True, null=True)
@@ -51,3 +55,27 @@ class Favorite(models.Model):
 
     def __str__(self):
         return f"{self.user} - {self.track}"
+
+
+@register_setting
+class FooterMenu(BaseGenericSetting):
+    title = models.CharField(_("Title"), max_length=255)
+    url = models.URLField(null=True, blank=True)
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("title"),
+                MultiFieldPanel(
+                    [FieldPanel("title"), FieldPanel("url")],
+                ),
+            ]
+        )
+    ]
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Footer Menu"
+        verbose_name_plural = "Footer Menus"
--- server/core/settings/base.py
+++ server/core/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "wagtail.search",
     "wagtail.admin",
     "wagtail",
+    "wagtail.contrib.settings",
     "modelcluster",
     "taggit",
     "django.contrib.admin",
