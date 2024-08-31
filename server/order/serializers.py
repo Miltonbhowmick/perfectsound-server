01fc84f5afa2aa85cbd0c4700153f54100141d25
miltonbhowmick <miltonbhowmick6@gmail.com>
2024-08-31 11:05:30 +0600

    fix: change create order mandatory fields

--- server/order/serializers.py
+++ server/order/serializers.py
@@ -12,22 +12,27 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class OrderCreateSerializer(serializers.Serializer):
-    first_name = serializers.CharField(max_length=255, required=True)
-    last_name = serializers.CharField(max_length=255, required=True)
-    phone_number = serializers.CharField(max_length=100, required=True)
-    price_plan = serializers.PrimaryKeyRelatedField(
-        queryset=PricePlan.objects.all(), required=True
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+    phone_number = serializers.CharField(
+        max_length=100, required=False, allow_blank=True, allow_null=True
     )
-    promo_code = serializers.PrimaryKeyRelatedField(
-        queryset=PromoCode.objects.all(), required=False, allow_null=True
+    price_plan = serializers.PrimaryKeyRelatedField(queryset=PricePlan.objects.all())
+    promo_code = serializers.CharField(
+        max_length=255, required=False, allow_blank=True, allow_null=True
     )
-    company = serializers.CharField(max_length=255, required=False, allow_null=True)
-    address1 = serializers.CharField(max_length=255, required=True)
-    address2 = serializers.CharField(max_length=255, required=False, allow_null=True)
-    country = serializers.CharField(max_length=255, required=True)
-    city = serializers.CharField(max_length=255, required=True)
-    street = serializers.CharField(max_length=255, required=True)
-    is_agreed_policy = serializers.BooleanField(required=True)
+    company = serializers.CharField(
+        max_length=255, required=False, allow_blank=True, allow_null=True
+    )
+    address1 = serializers.CharField(max_length=255)
+    address2 = serializers.CharField(
+        max_length=255, required=False, allow_blank=True, allow_null=True
+    )
+    country = serializers.CharField(max_length=255)
+    city = serializers.CharField(max_length=255)
+    state = serializers.CharField(max_length=255)
+    zip_code = serializers.CharField(max_length=255)
+    is_agreed_policy = serializers.BooleanField()
 
     def create(self, validated_data):
         user = self.context["user"]
