190ca1eee9d39db192b1633285243ff8ee4ffc1f
miltonbhowmick <miltonbhowmick6@gmail.com>
2024-09-05 01:08:31 +0600

    feat: add tracks list search query by title

--- server/music/filters.py
+++ server/music/filters.py
@@ -4,6 +4,11 @@ from .models import *
 
 
 class TrackFilter(filters.FilterSet):
+    title = filters.CharFilter(field_name="title", lookup_expr="icontains")
+
     class Meta:
         model = Track
-        fields = {"category__slug": ["exact"], "sub_category__slug": ["exact"]}
+        fields = {
+            "category__slug": ["exact"],
+            "sub_category__slug": ["exact"],
+        }
