69499df1bd97f403ec99b2b0eaac83d6d3f0adc5
miltonbhowmick <miltonbhowmick6@gmail.com>
2024-09-04 20:25:02 +0600

    feat: add api user favourite track delete

--- server/common/views.py
+++ server/common/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
 from .models import *
 from .serializers import *
@@ -25,3 +27,11 @@ class PublicFavouriteViewset(viewsets.ModelViewSet):
     def get_queryset(self):
         qs = self.queryset.filter(user=self.request.user)
         return qs
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_204_NO_CONTENT)
